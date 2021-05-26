692b732d2643d35dec92915ae4f005046adbe626
Sean O'Brien <sean.obrien74@icloud.com>
2021-05-26 18:54:19 -0500

    jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 
@@ -48,8 +50,23 @@ router.post('/', [
       user.password = await bcrypt.hash(password, salt);
       await user.save(); 
 
-      res.send('User registered...');
-    
+      // create payload
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 }, // update to 3600 on production
+        (err, token) =>
+        {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
   } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
